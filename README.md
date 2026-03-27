ab49e7b3d554bb0e395ec938e5f768ce1ad2157d
Pedro Fernandes <112490400+pmcf-percona@users.noreply.github.com>
2026-03-27 10:44:55 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+This is a dummy prototype to help us with feedback on ideas for the PMM Advisors feature.
--- vite.config.ts
+++ vite.config.ts
@@ -2,5 +2,6 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
+  base: '/advisors-rebirth/',
   plugins: [react()],
 });
