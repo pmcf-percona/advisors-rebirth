6df63e85078062e651bd3f95f3fe8ed5728a8662
freenandes <uuuuurso@gmail.com>
2026-03-27 10:56:25 +0000

    fix deployment

--- src/main.tsx
+++ src/main.tsx
@@ -6,7 +6,7 @@ import './index.css';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/advisors-rebirth">
       <App />
     </BrowserRouter>
   </StrictMode>,
